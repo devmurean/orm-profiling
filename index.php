db37114fe874d10c00bbd0369d7f85e33625f471
Dev Murean <dev.murean@gmail.com>
2024-05-09 20:06:19 +0700

    Revert using Symfony\Dotenv

--- index.php
+++ index.php
@@ -4,8 +4,7 @@ require realpath('.') . "/vendor/autoload.php";
 use Pecee\SimpleRouter\SimpleRouter;
 use Symfony\Component\Dotenv\Dotenv;
 
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__ . '/.env');
+(new Dotenv())->load(__DIR__ . '/.env');
 
 require_once './routes.php';
 
